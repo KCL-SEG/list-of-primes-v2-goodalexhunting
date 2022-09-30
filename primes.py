9afe318e83b6cd97f24a89c48a4c6449df941b82
Alex San <goodalexhunting@gmail.com>
2022-09-30 12:27:26 +0100

    Attempt #1

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+def isprime(n):  
+    for i in range(2,n):
+        if (n%i) == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
-    list = []
+    # try:
+    if number_of_primes < 1:
+        raise ValueError("Number of prime numbers must be greater than one.")   
+    i=2 
+    list=[] 
+    while(len(list) != number_of_primes):  
+        if(isprime(i)):  
+            list.append(i) 
+        i+=1
     return list
+    # except ValueError:
+    #     print("Input value must be an integer")
+
+print(primes(-2))
